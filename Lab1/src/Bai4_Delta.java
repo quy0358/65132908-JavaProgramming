2f097741c0a7af4f4d042e9b220675d25e3e9866
quy0358 <153436485+quy0358@users.noreply.github.com>
2026-01-22 16:32:32 +0700

    chỉnh lại format code cho 4 bài tập lab 1

--- Lab1/src/Bai4_Delta.java
+++ Lab1/src/Bai4_Delta.java
@@ -3,22 +3,16 @@ import java.util.Scanner;
 public class Bai4_Delta {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         // Nhập các hệ số
         System.out.print("Nhập a: ");
         double a = sc.nextDouble();
-
         System.out.print("Nhập b: ");
         double b = sc.nextDouble();
-
         System.out.print("Nhập c: ");
         double c = sc.nextDouble();
-
         // Tính delta
         double delta = b * b - 4 * a * c;
-
         System.out.println("Delta = " + delta);
-
         // Tính và xuất căn delta (nếu delta >= 0)
         if (delta >= 0) {
             double canDelta = Math.sqrt(delta);
@@ -26,7 +20,6 @@ public class Bai4_Delta {
         } else {
             System.out.println("Delta âm, không có căn bậc hai thực.");
         }
-
         sc.close();
     }
 }
